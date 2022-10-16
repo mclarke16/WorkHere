e5decca68002826847b1e045a3c6159f7eaccebb
mclarke16 <107970544+mclarke16@users.noreply.github.com>
2022-10-15 20:53:51 -0500

    Update utilities.h

--- utilities.h
+++ utilities.h
@@ -21,7 +21,7 @@ bool checkNodeID(Log *simList)
     size_t len = sizeof(simList);
     for (i; i < len; ++i)
     {
-        if (simList[i].start_node == simList.start_node[i])
+        if (simList[i].start_node == simList->start_node[i])
         {
             return true;
 
