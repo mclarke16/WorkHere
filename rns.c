d1392a0adbec7030b725af5c5fc48bb255c91902
mclarke16 <107970544+mclarke16@users.noreply.github.com>
2022-10-15 22:37:12 -0500

    Update rns.c

--- rns.c
+++ rns.c
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
             else
             {
                 buildNode(nodeList, buffer, fp);
-                printAllRep(checkNodeID(simList));
+                printAllRep(simList);
                 fclose(fp);
                 free(nodeList->conList);
             }
