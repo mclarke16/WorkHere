69b1241930964eadd053f0711368953ee0d3c256
mclarke16 <107970544+mclarke16@users.noreply.github.com>
2022-10-15 22:38:07 -0500

    Update rns.c

--- rns.c
+++ rns.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include "node.h"
-#include "utilities.h"
 #define BUFF_SIZE 1024
 
 int main(int argc, char **argv)
@@ -34,7 +33,6 @@ int main(int argc, char **argv)
             else
             {
                 buildNode(nodeList, buffer, fp);
-                printAllRep(simList);
                 fclose(fp);
                 free(nodeList->conList);
             }
@@ -48,6 +46,7 @@ int main(int argc, char **argv)
             else
             {
                 build_sim(simList, buffer, sim);
+                printAllRep(simList)
                 fclose(sim);
             }
             break;
